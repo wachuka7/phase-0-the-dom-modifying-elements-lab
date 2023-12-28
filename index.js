fef267fc05a22d6d63505e21ce0be861449f009f
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-28 23:37:55 +0300

    Modify DOM Elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+//queryselector to get the id main inside the main inside element main
+// use remove to remove the id main
+document.querySelector('main#main').remove();
+// use createElement to create a h1 
+//declare variable newHeader 
+const newHeader= document.createElement('h1');
+//setting the newHeader id as victory
+newHeader.id = 'victory';
+//setting a message in the h1, newHeader
+newHeader.innerHTML = "Racchael is the champion 2024";
