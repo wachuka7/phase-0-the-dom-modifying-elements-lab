85ad6e863603aa440f304bbe25679f25e4cbf61f
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-29 16:38:55 +0300

    Modify Elements

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//queryselector to get the id main inside the main inside element main
+//queryselector to get the id main, inside element main
 // use remove to remove the id main
 document.querySelector('main#main').remove();
 // use createElement to create a h1 
@@ -7,4 +7,4 @@ const newHeader= document.createElement('h1');
 //setting the newHeader id as victory
 newHeader.id = 'victory';
 //setting a message in the h1, newHeader
-newHeader.innerHTML = "Racchael is the champion 2024";
+newHeader.innerHTML = "Rachael is the champion 2024";
